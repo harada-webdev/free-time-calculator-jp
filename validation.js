1050748face5738a8b155a9cc2e6086139e0aac3
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2024-12-27 21:50:31 +0900

    Modify timeRegex

--- validation.js
+++ validation.js
@@ -84,7 +84,7 @@ function isValidTimeFormat(input) {
 }
 
 const isValidTime = (input) => {
-  const timeRegex = /^([0-9]?[0-9]|2[0-3]):([0-5]?[0-9])$/;
+  const timeRegex = /^([0-1]?[0-9]|2[0-3]):([0-5]?[0-9])$/;
   return timeRegex.test(input);
 };
 
