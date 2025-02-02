545e556561a9ab8e03e07683d06a603e4a8aa7f3
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2025-02-02 23:03:48 +0900

    Modify isValidTime

--- validation.js
+++ validation.js
@@ -84,8 +84,7 @@ function isValidTimeFormat(input) {
 }
 
 const isValidTime = (input) => {
-  const timeRegex = /^([0-1]?[0-9]|2[0-3]):([0-5]?[0-9])$/;
-  return timeRegex.test(input);
+  return /^([0-1]?[0-9]|2[0-3]):([0-5]?[0-9])$/.test(input);
 };
 
 function isValidTotalTime(input, times) {
